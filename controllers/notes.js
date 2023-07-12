f8316fd3f966f8d83a73a88e92d68cf1681a85a2
Irena <irena.holjevac@serengetitech.com>
2023-07-12 09:40:37 +0200

    run through user admin & token auth

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,21 @@
 const notesRouter = require('express').Router()
+const User = require('../models/user')
 const Note = require('../models/note')
+const jwt = require('jsonwebtoken')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => response.json(notes))
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(notes)
 })
 
 notesRouter.get('/:id', async (request, response) => {
@@ -16,18 +29,23 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: 'Content missing'
-    })
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
+  const user = await User.findById(decodedToken.id)
+  // const user = await User.findById(body.userId)
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id, // ne znam da li kod njih na webu ima
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
@@ -36,15 +54,14 @@ notesRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response) => {
   const { content, important } = request.body
-  Note.findByIdAndUpdate(
+  const updatedNote = await Note.findByIdAndUpdate(
     request.params.id,
     { content, important },
     { new: true, runValidators: true, context: 'query' }
   )
-    .then(updatedNote => response.json(updatedNote))
-    .catch(error => next(error))
+  response.json(updatedNote)
 })
 
 module.exports = notesRouter
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,12 +3,47 @@ const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const bcrypt = require('bcrypt')
 
+const User = require('../models/user')
 const Note = require('../models/note')
 
 beforeEach(async () => {
   await Note.deleteMany({})
   await Note.insertMany(helper.initialNotes)
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
 })
 
 describe('when there is initially some notes saved', () => {
@@ -72,6 +107,7 @@ describe('addition of a new note', () => {
     const newNote = {
       content: 'async/await simplifies making async calls',
       important: true,
+      userId: '64ad84caec97efe35d328a7b', // kod njih na webu nema
     }
 
     await api
